5c2588451d24fe6db06640d14ab034b02f7b618c
Gustavo Coelho da Silva <gcoelh5@gmail.com>
2026-06-28 14:25:54 -0300

    adição do gitignore
    Adiciona .gitignore ao projeto. Ignora pastas de ambiente virtual e logs no .gitignore

--- principal/main/verificadores.py
+++
@@ -1,8 +0,0 @@
-
-def verificarnumero(prompt=">>>>"):
-    while True:
-        try:
-            n = int(input(prompt))
-            return n
-        except:
-            print("Digite apenas numeros")
