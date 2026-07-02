d1343363d322c1d13e6aeefca1fa9d0dd65c5d0d
Gustavo Coelho da Silva <gcoelh5@gmail.com>
2026-07-02 18:44:43 -0300

    Ajuste na exibir dos Status
    Ajuste na exibição dos status iniciais

--- src/main/jogador.py
+++ src/main/jogador.py
@@ -1,5 +1,4 @@
 from time import sleep
-import random
 from ilustração import limpartela, cabeçalho, linha
 
 # Variáveis globais
@@ -15,12 +14,12 @@ def criar_personagem():
 
     ps = nm
     sleep(1.5)
-    cabeçalho(f"Bem vindo ao futuro {ps}", sinal="*")
+    cabeçalho(f"\t\t\tBem vindo ao futuro {ps}", sinal="**")
     sleep(2)
     limpartela()
 
 
-def atributos(vida=100, defesa=4, ataque=20, cura=5, xp=10, nivel=1):
+def atributos(vida=100, defesa=4, ataque=20, cura=10, xp=10, nivel=1):
     global personagem, ps
 
     personagem["nome"] = ps
@@ -33,14 +32,18 @@ def atributos(vida=100, defesa=4, ataque=20, cura=5, xp=10, nivel=1):
 
 
 def mostrar_atributos():
-    cabeçalho("\033[36mSTATUS INICIAL\033[0m", sinal="=+")
+    cabeçalho("STATUS INICIAL", sinal="=")
     sleep(2)
+    largura = 35
+    cor1 = "\033[33m"
+    cor2 = "\033[1;34m"
+    reset = "\033[0m"
     for k, v in personagem.items():
-        print(f"\033[33m{k.capitalize()}:\033[0m \033[1;34m{v}\033[0m")
-        linha(tp="-")
+        print(f"{cor1}{k.capitalize():^20}{reset}: {cor2}{v:^10}{reset}")
+        linha(tp="--")
         sleep(0.3)
-    linha("=")
-    sleep(1)
+    linha("==")
+    sleep(4)
 
 
 def iniciar_jogador():
@@ -58,3 +61,4 @@ def get_ps():
 
 def get_personagem():
     return personagem
+
