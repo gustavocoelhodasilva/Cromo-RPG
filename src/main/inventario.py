afad77af8031c17c01ead760e06bf4d692e4948a
Gustavo Coelho da Silva <gcoelh5@gmail.com>
2026-07-04 19:12:05 -0300

    comecei a fazer o inventario

--- src/main/inventario.py
+++ src/main/inventario.py
@@ -1,5 +1,47 @@
 from ilustração import cabeçalho,limpartela
 from verificadores import verificarnumero
+from jogador import atributos,mostrar_atributos,get_personagem
 
-def abrir_inventario():
-    cabeçalho()
+atributos()
+personagem = get_personagem()
+
+
+
+def pocoes(*seringas):
+
+    armazenamento = list(seringas)
+    lista = []
+    
+
+    for caixa in armazenamento:
+       tiposdeseringa = {}
+       tiposdeseringa["nome"] = caixa[0]
+       tiposdeseringa["tipo"] = caixa [1]
+       tiposdeseringa["efeito"] = caixa[2]
+       lista.append(tiposdeseringa)
+    
+    nomes = []
+    for item in lista:
+        if item["nome"]:
+           nomes.append(f"{item.get('nome')}") 
+ 
+
+
+
+
+
+
+
+def inventario():
+    limpartela()
+    cabeçalho("INVENTARIO")
+
+    categorias = {
+        "Seringas":"em breve",
+        "Armas": "em breve",
+        "Itens": "em breve",
+    }
+    print(categorias)
+
+
+
